a9f736cc0bd5531a669c3d9637727837d4558adf
Daniel Tieber <daniel.tieber@redbull.com>
2024-12-27 13:50:23 +0100

    refactor: rename move to move_east

--- lib/toy_robot/robot.ex
+++ lib/toy_robot/robot.ex
@@ -1,18 +1,18 @@
 defmodule ToyRobot.Robot do
   @doc """
-  Moves the robot forward one space.
+  Moves the robot east one space.
 
   ## Examples
       iex> alias ToyRobot.Robot
       ToyRobot.Robot
       iex> robot = %{position: 0}
       %{position: 0}
-      iex> robot |> Robot.move
+      iex> robot |> Robot.move_east
       %{position: 1}
-      iex> robot |> Robot.move |> Robot.move |> Robot.move
+      iex> robot |> Robot.move_east |> Robot.move_east |> Robot.move_east
       %{position: 3}
   """
-  def move(robot) do
+  def move_east(robot) do
     %{position: robot.position + 1}
   end
 end
