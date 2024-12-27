d4c5a79c09b6a10310a52fd491cd55b287003dd9
Daniel Tieber <daniel.tieber@redbull.com>
2024-12-27 13:58:00 +0100

    feat: add method to move robot west one space

--- lib/toy_robot/robot.ex
+++ lib/toy_robot/robot.ex
@@ -15,4 +15,21 @@ defmodule ToyRobot.Robot do
   def move_east(robot) do
     %{position: robot.position + 1}
   end
+
+  @doc """
+  Moves the robot west one space.
+
+  ## Examples
+      iex> alias ToyRobot.Robot
+      ToyRobot.Robot
+      iex> robot = %{position: 0}
+      %{position: 0}
+      iex> robot |> Robot.move_west
+      %{position: -1}
+      iex> robot |> Robot.move_west |> Robot.move_west |> Robot.move_west
+      %{position: -3}
+  """
+  def move_west(robot) do
+    %{position: robot.position - 1}
+  end
 end
