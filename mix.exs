850c9ef80c710010c06c8baa3029f1f3df137c91
Daniel Tieber <daniel.tieber@redbull.com>
2024-12-27 14:17:30 +0100

    build: install `ex_doc`
    to generate docs with `mix docs`

--- mix.exs
+++ mix.exs
@@ -19,8 +19,9 @@ defmodule ToyRobot.MixProject do
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  def deps do
     [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
