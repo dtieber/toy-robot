fdb4878e4c0149cdcd1405690dce9ec5b623054e
Daniel Tieber <daniel.tieber@redbull.com>
2024-12-28 09:58:00 +0100

    feat: verify placement when robot is placed

--- lib/toy_robot/simulation.ex
+++ lib/toy_robot/simulation.ex
@@ -25,14 +25,57 @@ defmodule ToyRobot.Simulation do
           robot: %Robot{north: 0, east: 0, facing: :north}
         }
       }
+
+  When the robot is placed in an invalid position:
+
+      iex> alias ToyRobot.{Robot, Table, Simulation}
+      [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> Simulation.place(table, %{north: 6, east: 0, facing: :north})
+      {
+        :error, :invalid_placement
+      }
   """
   def place(table, placement) do
-    {
-      :ok,
-      %__MODULE__{
-        table: table,
-        robot: struct(Robot, placement)
+    if table |> Table.valid_position?(placement) do
+      {
+        :ok,
+        %__MODULE__{
+          table: table,
+          robot: struct(Robot, placement)
+        }
       }
-    }
+    else
+      {:error, :invalid_placement}
+    end
+  end
+
+  @doc """
+  Moves the robot forward one space in the direction it is facing, unless that position is past the boundaries of the table.
+
+  ## Examples
+
+  ### A valid movement
+
+      iex> alias ToyRobot.{Robot, Table, Simulation}
+      [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> simulation = %Simulation{
+      ...>  table: table,
+      ...>  robot: %Robot{north: 0, east: 0, facing: :north}
+      ...> }
+      iex> simulation |> Simulation.move
+      {
+        :ok,
+        %Simulation{
+          table: table,
+          robot: %Robot{north: 1, east: 0, facing: :north}
+        }
+      }
+  """
+  def move(%{robot: robot} = simulation) do
+    {:ok, %{simulation | robot: robot |> Robot.move}}
   end
 end
